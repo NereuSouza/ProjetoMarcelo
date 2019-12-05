705adf60f5800629de22aa7615134ef236cc9143
Nereu <Nereu@DESKTOP-8IJ1EON>
2019-12-04 22:47:45 -0300

    metodo insert banco

--- TrabalhandoArquivoRedeCard/src/projeto/LerArquivo.java
+++ TrabalhandoArquivoRedeCard/src/projeto/LerArquivo.java
@@ -5,47 +5,58 @@ import java.io.File;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.text.ParseException;
-
 
 public class LerArquivo {
 
 	public static void main(String[] args) throws ParseException, IOException {
 		// TODO Auto-generated method stub
-		
+
+		/*
+		 * Conexao con = new Conexao();
+		 * 
+		 * Statement st = con.getConexao().createStatement();
+		 * 
+		 * st.executeQuery("select * from motorista");
+		 * 
+		 * ResultSet rs = st.getResultSet();
+		 * 
+		 */
+
 		Linha30Dao linha30 = new Linha30Dao();
 		Linha32Dao linha32 = new Linha32Dao();
 		Linha34Dao linha34 = new Linha34Dao();
-		
+
 		File arquivo1 = new File("C:/Users/NEREU/Downloads/NEFI_TESTE_1908.TXT");
 		FileReader fr = new FileReader(arquivo1);
 		BufferedReader br = new BufferedReader(fr);
 
 		while (br.ready()) {
 
-		String linha = br.readLine();
-		String tipoDeRegistro = linha.substring(0,3);
-		
-		switch (tipoDeRegistro) {
-		case "030":
-			
-			linha30.insertLinhaBanco(linha);
-		
-		case "032":
-			
-			linha32.insertLinhaBanco(linha);
-			
-		case "034":
-			
-			linha34.insertLinhaBanco(linha);
-			
-		}
-		
-		
+			String linha = br.readLine();
+			String tipoDeRegistro = linha.substring(0, 3);
+
+			switch (tipoDeRegistro) {
+			case "030":
+
+				linha30.insertLinhaBanco(linha);
+
+			case "032":
+
+				linha32.insertLinhaBanco(linha);
+
+			case "034":
+
+				linha34.insertLinhaBanco(linha);
+
+			}
+
 		}
 		br.close();
 		fr.close();
-		
+
 		System.out.println("fim");
 
 	}
